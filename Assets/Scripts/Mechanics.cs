d3fac254dd48c7545334bac5daaedb6df1e0999f
Rizwan Saleem <rizstar125@gmail.com>
2024-07-27 18:00:32 +0500

    Ragdoll Mechanics

---
+++ Assets/Scripts/Mechanics.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mechanis : MonoBehaviour
+{
+    [SerializeField]
+    private float targetRotation;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRotation, force * Time.fixedDeltaTime));
+    }
+
+}
